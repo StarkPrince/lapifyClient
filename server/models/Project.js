d0c08ae4ff71d3b1a2f082dbdecc9c3bb042d623
Prince Raj <games.princeraj@gmail.com>
2021-09-12 23:19:04 +0530

    dan dana dan

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,1 +1,12 @@
-import mon
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const ProjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    timeStamps: [Number]
+});
+
+export default mongoose.model('Project', ProjectSchema);
