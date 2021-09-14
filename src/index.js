061f82f3180bf25449d5e76bc067d4b86ec12a18
Prince Raj <games.princeraj@gmail.com>
2021-09-14 22:51:41 +0530

    removed the extension

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
 ));
 
 
